6627e6744725005068da0368038d8ae54146a4ae
sangmyeongsheon <deu09077@dgist.ac.kr>
2025-06-29 01:22:37 +0900

    change_made

--- src/pages/oj/App.vue
+++ src/pages/oj/App.vue
@@ -7,7 +7,9 @@
       </transition>
       <div class="footer">
         <p v-html="website.website_footer"></p>
-        <p>Powered by <a href="https://github.com/QingdaoU/OnlineJudge">OnlineJudge</a>
+        <p>
+          Powered by
+          <a href="https://github.com/sangmyeongsheon">MadebySangmyeong</a>
           <span v-if="version">&nbsp; Version: {{ version }}</span>
         </p>
       </div>
@@ -17,63 +19,61 @@
 </template>
 
 <script>
-  import { mapActions, mapState } from 'vuex'
-  import NavBar from '@oj/components/NavBar.vue'
+import { mapActions, mapState } from "vuex";
+import NavBar from "@oj/components/NavBar.vue";
 
-  export default {
-    name: 'app',
-    components: {
-      NavBar
+export default {
+  name: "app",
+  components: {
+    NavBar,
+  },
+  data() {
+    return {
+      version: process.env.VERSION,
+    };
+  },
+  created() {
+    try {
+      document.body.removeChild(document.getElementById("app-loader"));
+    } catch (e) {}
+  },
+  mounted() {
+    this.getWebsiteConfig();
+  },
+  methods: {
+    ...mapActions(["getWebsiteConfig", "changeDomTitle"]),
+  },
+  computed: {
+    ...mapState(["website"]),
+  },
+  watch: {
+    website() {
+      this.changeDomTitle();
     },
-    data () {
-      return {
-        version: process.env.VERSION
-      }
+    $route() {
+      this.changeDomTitle();
     },
-    created () {
-      try {
-        document.body.removeChild(document.getElementById('app-loader'))
-      } catch (e) {
-      }
-    },
-    mounted () {
-      this.getWebsiteConfig()
-    },
-    methods: {
-      ...mapActions(['getWebsiteConfig', 'changeDomTitle'])
-    },
-    computed: {
-      ...mapState(['website'])
-    },
-    watch: {
-      'website' () {
-        this.changeDomTitle()
-      },
-      '$route' () {
-        this.changeDomTitle()
-      }
-    }
-  }
+  },
+};
 </script>
 
 <style lang="less">
+* {
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+  box-sizing: border-box;
+}
 
-  * {
-    -webkit-box-sizing: border-box;
-    -moz-box-sizing: border-box;
-    box-sizing: border-box;
+a {
+  text-decoration: none;
+  background-color: transparent;
+  &:active,
+  &:hover {
+    outline-width: 0;
   }
+}
 
-  a {
-    text-decoration: none;
-    background-color: transparent;
-    &:active, &:hover {
-      outline-width: 0;
-    }
-  }
-
-
-  @media screen and (max-width: 1200px) {
+@media screen and (max-width: 1200px) {
   .content-app {
     margin-top: 160px;
     padding: 0 2%;
@@ -87,16 +87,14 @@
   }
 }
 
-  .footer {
-    margin-top: 20px;
-    margin-bottom: 10px;
-    text-align: center;
-    font-size: small;
-  }
+.footer {
+  margin-top: 20px;
+  margin-bottom: 10px;
+  text-align: center;
+  font-size: small;
+}
 
-  .fadeInUp-enter-active {
-    animation: fadeInUp .8s;
-  }
-
-
+.fadeInUp-enter-active {
+  animation: fadeInUp 0.8s;
+}
 </style>
